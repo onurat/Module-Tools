f377e38ed2b419fb4d92ad8c305ee69b1581f08d
Onur Atas <114289826+onurat@users.noreply.github.com>
2026-04-10 19:26:40 +0100

    Add jq exercises solutions

--- jq/script-11.sh
+++ jq/script-11.sh
@@ -5,3 +5,5 @@ set -euo pipefail
 # The input for this script is the scores.json file.
 # TODO: Write a command to output the total of adding together all scores from all games from all players.
 # Your output should be exactly the number 164.
+
+jq '[.[].scores[]] | add' scores.jsons
